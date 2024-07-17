3e908143fc122bc2ffb74527ffbd7c834469fe74
Adn1217 <adn1217@hotmail.com>
2024-07-17 17:43:39 -0500

    transfer function fixed to receive owner

--- CriptoXMC/transfer.sol
+++ CriptoXMC/transfer.sol
@@ -12,23 +12,36 @@ interface IERC20 {
 }
 
 contract MainXM {
-    IERC20 public token;
-    constructor(address _token) {
-        token = IERC20(_token);
+    IERC20 public token; // Token tipo IERC20, banco o "XMCOP".
+    address public owner;
+
+    constructor(address _token, address initialOwner) {
+        owner = initialOwner;
+        token = IERC20(_token); // Se inicializa variable Token (direción del contrato).
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only owner");
+        _;
     }
 
     mapping (uint256 => uint256) public transfers;
     function deposit(
         address _sender,
         uint256 _amount,
-        uint256 _idTrx
+        uint256 _idTrx // Id de transacción.
     ) external {
         bool transferSuccess = token.transferFrom(
             _sender,
-            address(this),
+            address(this), // Dirección de este contrato.
             _amount
         );
+    require(transferSuccess, "Transfer to contract failed");
         transfers[_idTrx] = _amount;
-        require(transferSuccess, "Transfer to contract failed");
+    }
+
+    function withdraw (address _receiver, uint256 _amount) public onlyOwner {
+        bool transferSuccess = token.transfer(_receiver, _amount);
+        require(transferSuccess, "Transfer to user failed");
     }
 }
